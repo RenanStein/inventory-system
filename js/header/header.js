38ed84643e450cfad36c76ff3899ba217174b57b
Renan Stein <renansteindev@gmail.com>
2026-07-17 20:48:17 -0300

    .
    .

--- js/header/header.js
+++ js/header/header.js
@@ -1,18 +1,26 @@
 export default function initUserHeader() {
     const textUser = document.querySelector('[data-user="usuario"]');
 
-    let user = JSON.parse(
-        localStorage.getItem('usuarioLogado')
-    );
+    if(localStorage.usuarioLogado){
+        const user = JSON.parse(localStorage.getItem('usuarioLogado'));
 
-    console.log(window.location.pathname)
-    if(window.location.pathname != '/index.html'){
-     if(user.situacao !== 'logado') {
-         window.location.href = './index.html';
-     }
+        if (user && user.situacao === 'logado') {
+        console.log('ok');
+    } else {
+        if (window.location.pathname !== '/index.html') {
+            window.location.href = './index.html';
+        }
+        return;
     }
 
-    if(textUser){
+    if (textUser) {
         textUser.innerText = user.usuario;
     }
+    } else {
+        if(window.location.pathname !== '/index.html'){
+        window.location.href = './index.html';
+        }
+    }
+
+    
 }
